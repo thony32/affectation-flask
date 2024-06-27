abac41dc8dc38f263973aa0015a274e68bc73830
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-06-27 22:20:38 +0300

    All dead

--- app/services/employee_service.py
+++ app/services/employee_service.py
@@ -1,7 +1,7 @@
 from app import db
 from app.models.employee import Employee
 
-# NOTE: Sign up
+
 def create_employee(username, first_name, last_name, job, password):
     new_employee = Employee(
         username=username, first_name=first_name, last_name=last_name, job=job
@@ -11,18 +11,18 @@ def create_employee(username, first_name, last_name, job, password):
     db.session.commit()
     return new_employee
 
-# NOTE: Authentication
+
 def authenticate_employee(username, password):
     employee = Employee.query.filter_by(username=username).first()
     if employee and employee.check_password(password):
         return employee
     return None
 
-# NOTE: get by id
+
 def get_employee_by_id(employee_id):
     return Employee.query.get(employee_id)
 
-# REFACTOR: update
+
 def update_employee(employee_id, **kwargs):
     employee = get_employee_by_id(employee_id)
     if employee:
@@ -31,7 +31,7 @@ def update_employee(employee_id, **kwargs):
         db.session.commit()
     return employee
 
-# NOTE: Delete
+
 def delete_employee(employee_id):
     employee = get_employee_by_id(employee_id)
     if employee:
