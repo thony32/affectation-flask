721cfa6ae16542d65a2119ac3025a2e3c85bae7f
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-06-27 22:19:15 +0300

    UPDATED .env example

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "ZEROVALO"
-    JWT_SECRET_KEY = "zerovaloJWT"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
